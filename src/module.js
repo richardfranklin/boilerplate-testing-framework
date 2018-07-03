f537432405d644bd9a7c2768faa66b48cfe9fa22
Richard Franklin <richard.franklin@gmail.com>
2018-07-03 11:10:06 +0100

    Initial Jasmine unit testing practice

--- src/module.js
+++ src/module.js
@@ -1,1 +1,17 @@
 export const testString = country => `I am from ${country}!`;
+
+export const testObj = {
+    testMethod: logString => {
+        console.log(`Hello ${logString}`);
+    }
+};
+
+export function Car() {
+    this.speed = 0;
+    this.accelerate = accSpeed => {
+        this.speed += accSpeed;
+    }
+    this.stop = () => {
+        this.speed = 0;
+    }
+}
--- tests/module.spec.js
+++ tests/module.spec.js
@@ -1,11 +1,50 @@
 import { testString } from '../src/module';
+import { testObj } from '../src/module';
+import { Car } from '../src/module';
 
 describe('module', function () {
 
     const countryMessage = testString('Sweden');
 
+    // BEFOREEACH ------------------------------------------------------------------------
+    beforeEach(() => { 
+        spyOn(testObj, 'testMethod'); 
+        testObj.testMethod('Bob');
+    });
+
+    // TEST ------------------------------------------------------------------------
     it('should display which country you\'re from', function () {
         expect(countryMessage).toBe('I am from Sweden!');
     });
-   
+
+    // TEST ------------------------------------------------------------------------
+    it('should call testMethod', () => {
+        expect(testObj.testMethod).toHaveBeenCalledWith('Bob');
+    });
+
+    // TEST ------------------------------------------------------------------------
+    it('Should be sped up by 50mph', () => {
+        var ford = new Car();
+        ford.accelerate(50);
+        expect(ford.speed).toEqual(50);
+    });
+
+    // TEST ------------------------------------------------------------------------
+    
+
+    // TEST ------------------------------------------------------------------------
+
+
+    // TEST ------------------------------------------------------------------------
+
+
+    // TEST ------------------------------------------------------------------------
+
+
+    // TEST ------------------------------------------------------------------------
+
+
+    // TEST ------------------------------------------------------------------------
+
+    
 });
